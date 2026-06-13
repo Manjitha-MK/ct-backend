6ac6f2c4cd4131945e5054b4a8d35585188f028b
Manjitha-MK <manjithataridu123@gmail.com>
2026-06-13 17:17:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/players", playerRoutes);
@@ -26,9 +26,26 @@ app.use("/api/dashboard", dashboardRoues);
 app.use("/api/matches", matchRoutes);
 app.use("/api/gallery", galleryRoutes);
 
-app.listen(process.env.PORT, () =>
-    console.log(`Server running on port ${process.env.PORT}`)
-);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
+
+
+
+// app.listen(process.env.PORT, () =>
+//     console.log(`Server running on port ${process.env.PORT}`)
+// );
 
 app.use((err, req, res, next) => {
   console.error(err);
